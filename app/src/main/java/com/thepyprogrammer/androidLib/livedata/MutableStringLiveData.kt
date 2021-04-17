1f89c59af27f0791e34eb2d308a2e206db55c5ba
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-17 22:46:01 +0800

    Update Gait Analyzer

--- app/src/main/java/com/thepyprogrammer/androidLib/livedata/MutableStringLiveData.kt
+++
@@ -1,9 +0,0 @@
-package com.thepyprogrammer.androidLib.livedata
-
-import androidx.lifecycle.MutableLiveData
-
-class MutableStringLiveData(init: String) : MutableLiveData<String>(init) {
-    override fun getValue(): String {
-        return super.getValue()!!
-    }
-}
---
+++ app/src/main/java/com/thepyprogrammer/androidlib/observable/ObservableUtils.kt
@@ -0,0 +1,10 @@
+package com.thepyprogrammer.androidlib.observable
+
+import androidx.databinding.Observable
+
+inline fun <reified T : Observable>
+        T.addOnPropertyChanged(crossinline callback: (T) -> Unit) =
+    object : Observable.OnPropertyChangedCallback() {
+        override fun onPropertyChanged(observable: Observable?, i: Int) =
+            callback(observable as T)
+    }.also { addOnPropertyChangedCallback(it) }
